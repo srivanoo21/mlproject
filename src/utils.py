ffa6bfc93710d6f50d3e6a72e5a366121108e1d5
Anoop Srivastava <srivanoo21@gmail.com>
2023-04-13 02:06:28 +0530

    app.py updated and predict pipeline completed

--- src/utils.py
+++ src/utils.py
@@ -54,3 +54,12 @@ def evaluate_models(xtrain, xtest, ytrain, ytest, models, params):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
