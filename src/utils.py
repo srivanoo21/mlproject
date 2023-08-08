2b3554e0f04586d07e269d1d2e74d735330acc0f
Anoop Srivastava <srivanoo21@gmail.com>
2023-08-09 01:19:57 +0530

    docker updated

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import sys
 
 import numpy as np 
 import pandas as pd
-import dill
+#import dill
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
@@ -36,7 +36,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-            #model.fit(X_train, y_train)  # Train model
+            # model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
